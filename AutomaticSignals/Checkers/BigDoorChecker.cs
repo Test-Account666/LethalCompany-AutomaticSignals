a0374c281273753dbab59612e8c741b53c81e624
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2024-03-24 21:30:36 +0100

    Add config, adjusted values

--- AutomaticSignals/Checkers/BigDoorChecker.cs
+++ AutomaticSignals/Checkers/BigDoorChecker.cs
@@ -1,16 +1,35 @@
+using BepInEx.Configuration;
 using GameNetcodeStuff;
 using UnityEngine;
 using Random = System.Random;
 
 namespace AutomaticSignals.Checkers;
 
+[InitializeConfig]
 public static class BigDoorChecker {
-    private const int MINIMUM_DOOR_COOL_DOWN = 15000;
-    private const int MAXIMUM_DOOR_COOL_DOWN = 23000;
-    private const int OPEN_CHANCE = 30;
-    private const int MALFUNCTION_CHANCE = 15;
+    private static ConfigEntry<int> _minimumDoorCoolDown = null!; // Default:  15000;
+    private static ConfigEntry<int> _maximumDoorCoolDown = null!; // Default:  23000;
+    private static ConfigEntry<int> _openChance = null!; // Default:  40;
+    private static ConfigEntry<int> _malfunctionChance = null!; // Default: 15;
     private static long _nextDoorOpen;
+
     private static readonly Random _Random = new();
+
+    public static void Initialize(ConfigFile configFile) {
+        _minimumDoorCoolDown = configFile.Bind("Blast Doors", "1. Minimum cooldown", 15000,
+            "Defines the minimum cooldown (in milliseconds) to wait before opening/closing a blast door");
+
+        _maximumDoorCoolDown = configFile.Bind("Blast Doors", "2. Maximum cooldown", 23000,
+            "Defines the maximum cooldown (in milliseconds) to wait before opening/closing a blast door");
+
+        _openChance = configFile.Bind("Blast Doors", "3. Open chance", 40,
+            new ConfigDescription("Defines the chance a blast door will open, if looked at",
+                new AcceptableValueRange<int>(0, 100)));
+
+        _malfunctionChance = configFile.Bind("Blast Doors", "4. Malfunction chance", 15,
+            new ConfigDescription("Defines the chance a blast door will close, if looked at",
+                new AcceptableValueRange<int>(0, 100)));
+    }
 
     public static void CheckForBigDoor(PlayerControllerB playerControllerB) {
         var ray = playerControllerB.interactRay;
@@ -26,9 +45,10 @@ public static class BigDoorChecker {
         if (currentTime < _nextDoorOpen)
             return;
 
-        _nextDoorOpen = currentTime + _Random.Next(MINIMUM_DOOR_COOL_DOWN, MAXIMUM_DOOR_COOL_DOWN);
+        _nextDoorOpen = currentTime + _Random.Next(_minimumDoorCoolDown.Value, _maximumDoorCoolDown.Value);
 
-        OpenOrMalfunctionDoor(accessibleObject, accessibleObject.isDoorOpen ? MALFUNCTION_CHANCE : OPEN_CHANCE);
+        OpenOrMalfunctionDoor(accessibleObject,
+            accessibleObject.isDoorOpen ? _malfunctionChance.Value : _openChance.Value);
     }
 
     private static TerminalAccessibleObject? GetBigDoor(Ray ray, float maxDistance) {

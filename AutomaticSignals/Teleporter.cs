272a0da780ddbfb4ab00bcdc7a81aa6453e29b76
TheBlackEntity <36412486+TheBlackEntity@users.noreply.github.com>
2024-03-28 21:32:02 +0100

    Make teleport client-side to fix issues

--- AutomaticSignals/Teleporter.cs
+++ AutomaticSignals/Teleporter.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq;
 using GameNetcodeStuff;
 using UnityEngine;
@@ -12,8 +13,7 @@ public static class Teleporter {
         if (teleporter is null)
             return;
 
-        StartOfRound.Instance.mapScreen.SwitchRadarTargetAndSync((int)playerControllerB.playerClientId);
-        teleporter.PressTeleportButtonServerRpc();
+        teleporter.StartCoroutine(BeamUpPlayer(playerControllerB, teleporter));
     }
 
     internal static void TeleportPlayerToLocation(PlayerControllerB playerControllerB, Vector3 position) {
@@ -36,5 +36,56 @@ public static class Teleporter {
         var teleporterList = Object.FindObjectsOfType<ShipTeleporter>();
 
         return teleporterList.FirstOrDefault(teleporter => teleporter.isInverseTeleporter == inverseTeleporter);
+    }
+
+
+    // Blatantly stolen from Zeekerss... Have to keep this until I find a better way
+    private static IEnumerator BeamUpPlayer(PlayerControllerB? playerToTeleport, ShipTeleporter? teleporter) {
+        if (teleporter is null)
+            yield break;
+
+        teleporter.shipTeleporterAudio.PlayOneShot(teleporter.teleporterSpinSFX);
+
+        if (playerToTeleport is null)
+            yield break;
+
+        if (playerToTeleport.deadBody is not null)
+            yield break;
+
+        teleporter.SetPlayerTeleporterId(playerToTeleport, 1);
+
+        playerToTeleport.beamUpParticle.Play();
+        playerToTeleport.movementAudio.PlayOneShot(teleporter.beamUpPlayerBodySFX);
+
+        yield return new WaitForSeconds(3f);
+
+        if (playerToTeleport.deadBody is not null)
+            yield break;
+
+        playerToTeleport.DropAllHeldItems();
+
+        var audioReverbPresets = Object.FindObjectOfType<AudioReverbPresets>();
+
+        // ReSharper disable once UseNullPropagation
+        if (audioReverbPresets is not null)
+            audioReverbPresets.audioPresets[3]
+                .ChangeAudioReverbForPlayer(playerToTeleport);
+
+        playerToTeleport.isInElevator = true;
+        playerToTeleport.isInHangarShipRoom = true;
+        playerToTeleport.isInsideFactory = false;
+        playerToTeleport.averageVelocity = 0.0f;
+        playerToTeleport.velocityLastFrame = Vector3.zero;
+
+        playerToTeleport.TeleportPlayer(teleporter.teleporterPosition.position, true, 160f);
+
+        teleporter.SetPlayerTeleporterId(playerToTeleport, -1);
+
+        teleporter.shipTeleporterAudio.PlayOneShot(teleporter.teleporterBeamUpSFX);
+
+        if (!GameNetworkManager.Instance.localPlayerController.isInHangarShipRoom)
+            yield break;
+
+        HUDManager.Instance.ShakeCamera(ScreenShakeType.Big);
     }
 }
